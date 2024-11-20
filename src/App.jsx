0154b1c96f26cf1bd74e7f9ef077cd2abd01f4b0
afyww <sivinaries@gmail.com>
2024-11-20 18:09:10 +0700

    upp

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import News from "./pages/News"
 import Profil1 from "./pages/Profil1"
 import Profil2 from "./pages/Profil2"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       
       <Routes>
         <Route path="/" element={<Home />} />
